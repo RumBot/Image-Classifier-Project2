e8776797863612877a512503919b293ec52068cd
RumBot <davisbr9@gmail.com>
2020-12-03 19:23:01 -0600

    Update train.py

--- train.py
+++ train.py
@@ -19,14 +19,14 @@ import master_utilities
 args = argparse.ArgumentParser(description='train.py')
 # Command Line ardguments
 
-ap.add_argument('data_dir', nargs='*', action="store", default="./flowers/")
-ap.add_argument('--gpu', dest="gpu", action="store", default="cpu")
-ap.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
-ap.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
-ap.add_argument('--dropout', dest = "dropout", action = "store", default = 0.5)
-ap.add_argument('--epochs', dest="epochs", action="store", type=int, default=5)
-ap.add_argument('--arch', dest="arch", action="store", default="vgg16", type = str)
-ap.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
+args.add_argument('data_dir', nargs='*', action="store", default="./flowers/")
+args.add_argument('--gpu', dest="gpu", action="store", default="cpu")
+args.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
+args.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
+args.add_argument('--dropout', dest = "dropout", action = "store", default = 0.5)
+args.add_argument('--epochs', dest="epochs", action="store", type=int, default=5)
+args.add_argument('--arch', dest="arch", action="store", default="vgg16", type = str)
+args.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
 
 # Collect and organize arguments
 parsedargs = args.parse_args()
